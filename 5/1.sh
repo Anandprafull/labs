78605a2608d7ebf5cd588872c48ba11055d33775
Prafull Anand <anandprafull007@gmail.com>
2025-11-11 18:31:56 +0530

    Add script to download files to a specified directory
    This script creates a directory and downloads specified files into it.

---
+++ 5/1.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Define the destination directory
+DEST_DIR="/code"
+
+# Create the directory if it doesn't exist
+if [ ! -d "$DEST_DIR" ]; then
+  sudo mkdir -p "$DEST_DIR"
+  echo "Directory $DEST_DIR created."
+fi
+
+# List of URLs to download
+URLs=(
+    "https://anandprafull.github.io/labs/5/aes.txt"
+    "https://anandprafull.github.io/labs/5/des.txt"
+    "https://anandprafull.github.io/labs/5/rsa.txt"
+    "https://anandprafull.github.io/labs/5/CaesarCipher.txt"
+    "https://anandprafull.github.io/labs/5/PlayFair.txt"
+    
+    "https://anandprafull.github.io/labs/5/tcp.txt"
+    "https://anandprafull.github.io/labs/5/udp.txt"
+    "https://anandprafull.github.io/labs/5/crc.txt"
+    "https://anandprafull.github.io/labs/5/ford.txt"
+
+
+)
+
+# Download each file and save it to the destination directory with sudo
+for URL in "${URLs[@]}"; do
+    sudo wget -P "$DEST_DIR" "$URL"
+done
+
+echo "All files downloaded successfully to $DEST_DIR"
+
+
+
+sudo chown -R $(whoami):$(whoami) /syst
+sudo ./download_files.sh
