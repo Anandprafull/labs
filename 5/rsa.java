db1caabd090a1cd6e1d77128517cf42e06b2bb5c
Prafull Anand <anandprafull007@gmail.com>
2025-11-11 18:05:19 +0530

    Implement RSA encryption and decryption example

---
+++ 5/rsa.java
@@ -0,0 +1,20 @@
+import java.security.*;
+import javax.crypto.Cipher;
+import java.util.Base64;
+
+public class rsa {
+        public static void main(String[] a) throws Exception{
+                KeyPairGenerator g = KeyPairGenerator.getInstance("RSA");
+                g.initialize(2048);
+                KeyPair k = g.generateKeyPair();
+                Cipher c = Cipher.getInstance("RSA");
+                String m ="Hello, RSA!";
+        
+                c.init(Cipher.ENCRYPT_MODE,k.getPublic());
+                String e=Base64.getEncoder().encodeToString(c.doFinal(m.getBytes()));
+                System.out.println("Enc:"+e);
+        
+                c.init(Cipher.DECRYPT_MODE,k.getPrivate());
+                System.out.println("Dec:"+new String(c.doFinal(Base64.getDecoder().dec>
+        }
+}
