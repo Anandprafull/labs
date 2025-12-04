53948f6b0188036e5dbbe1b0c5ea3cdc85e180ea
Prafull Anand <anandprafull007@gmail.com>
2025-12-04 21:04:31 +0530

    Implement voter eligibility form and servlet
    Added HTML form for voter eligibility check and servlet for processing requests.

---
+++ see/P11.java
@@ -0,0 +1,43 @@
+//Code: index.html (Form)
+<html>
+<head>
+<title>VoterApp</title>
+</head>
+<body>
+<form action= "http://localhost:8080/CheckAge/check" method="get">
+<fieldset style="width:20%; background-color:#80ffcc">
+<table>
+<tr><td>Name</td><td><input type="text" name="name"></td></tr>
+<tr><td>Age</td><td><input type="text" name="age"></td></tr>
+<tr><td></td>
+<td><input type = "submit" value="Check Eligibility"></td></tr>
+</table>
+</fieldset>
+</form>
+</body>
+</html>
+
+//Code: VoterSrv.java (Servlet)
+// Must be compiled and deployed within a Java EE/Servlet container setup
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class VoterSrv extends HttpServlet {
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        res.setContentType("text/html");
+        PrintWriter pw = res.getWriter();
+
+        String name = req.getParameter("name");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        if (age >= 18) {
+            pw.println("<font color='green' size='4'>Welcome " + name + " to this site</font>");
+        } else {
+            pw.println("<font color='red' size='4'>Hello " + name + ", you are not authorized to visit the site</font>");
+        }
+
+        pw.println("<br><br><a href= 'index.html'>back</a>");
+        pw.close();
+    }
+}
